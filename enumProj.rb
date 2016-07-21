8db27d6a25068e2b50dadd74196b2b409269a3fb
Calvin Hamilton <chamilton025@gmail.com>
2016-07-21 11:12:36 -0400

    Finished enumProj with tests at the bottom

--- enumProj.rb
+++ enumProj.rb
@@ -2,30 +2,33 @@
 
 module Enumerable
     def my_each
+    	return self unless block_given?
         for i in 0...self.size do
             yield(self[i])
         end
     end
 
     def my_each_with_index
+    	return self unless block_given?
         for i in 0...self.size do
             yield(self[i], i)
         end
     end
 
     def my_select
+    	return self unless block_given?
         newArr = []
-        self.my_each do |n|
+        my_each do |n|
             if yield(n)
                 newArr.push(n)
             end
         end
-        newArr
+        return newArr
     end
 
     def my_all?
         allT = true
-        self.my_each do |n|
+        my_each do |n|
             if !yield(n)
                 allT = false
             end
@@ -35,7 +38,7 @@ module Enumerable
 
     def my_any?
         anyT = false
-        self.my_each do |n|
+        my_each do |n|
             if yield(n)
                 anyT = true
             end
@@ -54,23 +57,86 @@ module Enumerable
     end
 
     def my_count(*item)
-        if item.length==0
-            if !block_given?
-                puts block_given?
-                puts "here instead"
-                return self.size
-            else
-                puts "here"
-                self.my_select do |n|
-                    puts n
-                    yield(n)
-                end.size
-            end
+        c = 0
+        if block_given?
+        	my_each do |n|
+        		if yield(n)
+    				c+=1
+    			end
+    		end
+    	elsif item.length==1
+    		my_each do |n|
+    			if n==item[0]
+    				c+=1
+    			end
+    		end
         else
-            self.my_select do |n|
-                n==item[0]
-            end.size
+        	c=size
         end
+        c
+    end
+    
+    def my_map(*args)
+    	return self if (!block_given?) && (args.size==0)
+    	newArr = []
+    	if args.size==1
+    		argProc = args[0]
+    		my_each do |n|
+    			newArr.push(argProc.call(n))
+    		end
+    	else
+	    	my_each do |n|
+	    		newArr.push(yield(n))
+	    	end
+	    end
+    	newArr
+    end
+    
+    def my_inject(*args)
+    	reuse = lambda do |init,sym,start|
+    		retVal = init
+    		for i in start...size do
+    			retVal = sym.to_proc.call(retVal,self[i])
+    		end
+    		retVal
+    	end
+    	if block_given?
+    		if args.size==1
+    			initial = args[0]
+    			return self unless initial.is_a?Integer
+    			
+    			retVal = initial
+    			my_each do |n|
+    				retVal = yield(retVal,n)
+    			end
+    			retVal
+    		elsif args.size==0
+    			retVal = self[0]
+    			for i in 1...size do
+    				retVal = yield(retVal,self[i])
+    			end
+    			retVal
+    		else
+    			return self
+    		end
+    	else
+    		if args.size==2
+    			initial = args[0]
+    			sym = args[1]
+    			return self unless (initial.is_a?Integer) && (sym.is_a?Symbol)
+    			
+    			retVal = reuse.call(initial, sym, 0)
+    			retVal
+			elsif args.size==1
+				sym = args[0]
+				return self unless sym.is_a?Symbol
+
+    			retVal = reuse.call(self[0], sym, 1)
+    			retVal
+    		else
+    			return self
+    		end
+    	end
     end
 end
 
@@ -90,8 +156,7 @@ end
 newArr = testArr.my_select do |n|
     n.even?
 end
-print newArr
-puts 
+puts newArr.join(",")
 
 ############################
 ans = testArr.my_all? do |n|
@@ -112,6 +177,35 @@ end
 puts ans
 
 ############################
-testArr.my_count do |n|
+ans = testArr.my_count do |n|
     n%2==0
 end
+puts ans
+
+############################
+newArr = testArr.my_map do |n|
+	n**n
+end
+puts newArr.join(",")
+
+############################
+ans =  %w{ cat sheep bear }.inject do |memo, word|
+   memo.length > word.length ? memo : word
+end
+puts ans
+
+############################
+def multiply_els(pArr)
+	ans = pArr.my_inject(:*)
+	puts ans
+end
+multiply_els([2,4,5])
+
+############################
+testProc = Proc.new do |n|
+	n**2
+end
+ans = testArr.my_map(testProc) do |n|
+	n**3
+end
+puts ans.join(",")
